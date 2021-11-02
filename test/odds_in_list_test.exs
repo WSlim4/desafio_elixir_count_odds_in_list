5ac93c70700e52860e8e1f73c0bdd996af531ea6
Wesley Lima <wesleyjoga500@gmail.com>
2021-11-02 04:14:23 -0300

    adicionando testes

--- test/odds_in_list_test.exs
+++ test/odds_in_list_test.exs
@@ -2,7 +2,10 @@ defmodule OddsInListTest do
   use ExUnit.Case
   doctest OddsInList
 
-  test "greets the world" do
-    assert OddsInList.hello() == :world
+  describe("call/l") do
+    test "count odds integers in list" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+      assert OddsInList.call(list) == 3
+    end
   end
 end
